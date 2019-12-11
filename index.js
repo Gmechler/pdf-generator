d35e2f6efbe9c252c96e5c6eb2849e5e47116d7e
Grayson Mechler <Graysonmechler@gmail.com>
2019-12-11 15:51:58 -0600

    playing with styling

--- index.js
+++ index.js
@@ -28,25 +28,34 @@ async function writePDF() {
     // console.log(data);
     function generateHTML() {
       const newHTML = `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Github Profile Info</title>
-</head>
-<body style="background-color: ${favColor};">
-  <img src="${imgURL}" height="300" width="300">
-  <h1>${name}</h1>
-  <h5>${bio}</h5>
-  <a href="${pageurl}">Github Page</a>
-  <div>${location}</div>
-  <h4>Repositories: ${repocount}</h4>
-  <h4>Following: ${following}</h4>
-  <h4>Followers: ${followers}</h4>
-  <h4>Location: ${location}</h4>
-</body>
-</html>`;
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+          <title>Github Profile Info</title>
+        </head>
+        <body style="box-sizing:content-box; background-color: ${favColor}">
+          <div
+            style="float: center; widows: 960px; background-color: #ffffff;
+          margin-left: auto;
+          margin-right: auto;
+          margin-top: 1in;
+          margin-bottom: 2in;
+          border: 2px solid #dddddd;"
+          >
+            <img src="${imgURL}" height="300" width="300" />
+            <h1 style="color: ${favColor};">${name}</h1>
+            <h5>${bio}</h5>
+            <a href="${pageurl}">Github Page</a>
+            <div>${location}</div>
+            <h4>Repositories: ${repocount}</h4>
+            <h4>Following: ${following}</h4>
+            <h4>Followers: ${followers}</h4>
+            <h4>Location: ${location}</h4>
+          </div>
+        </body>
+      </html>`;
       return newHTML;
     }
     const html = await generateHTML();
